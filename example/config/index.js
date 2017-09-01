26717a60c50cb776191cf812845c6415aee2cf8b
artemsky <mr.artemsky@gmail.com>
2017-09-01 15:25:00 +0300

    [demo] add gzip, chage public path

--- example/config/index.js
+++ example/config/index.js
@@ -7,13 +7,13 @@ module.exports = {
     index: path.resolve(__dirname, '../../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-snotify/',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
+    productionGzip: true,
     productionGzipExtensions: ['js', 'css'],
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
