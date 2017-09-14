e641c81873c257c420f76aec2089e648ff2ddf1b
artemsky <mr.artemsky@gmail.com>
2017-09-14 12:16:31 +0300

    add service static typings

--- src/typings/snotify/snotify.service.d.ts
+++ src/typings/snotify/snotify.service.d.ts
@@ -5,19 +5,18 @@ import { SnotifyConfig } from './interfaces/SnotifyConfig.interface';
 /**
  * SnotifyService - create, remove, config toasts
  */
-
-export interface SnotifyService extends Vue {
+export declare class SnotifyService extends Vue {
   notifications: SnotifyToast[];
   readonly config: SnotifyConfig;
   readonly options: SnotifyOptions;
-  get(id: number): SnotifyToast;
-  getAll(id: number): SnotifyToast[];
-  emit(): void;
-  setConfig(config: SnotifyConfig, options?: SnotifyOptions): void;
-  add(toast: SnotifyToast);
-  remove(id?: number): void;
-  clear(): void;
-  create(snotify: SnotifyToast): number;
+  static get(id: number): SnotifyToast;
+  static getAll(id: number): SnotifyToast[];
+  static emit(): void;
+  static setConfig(config: SnotifyConfig, options?: SnotifyOptions): void;
+  static add(toast: SnotifyToast);
+  static remove(id?: number): void;
+  static clear(): void;
+  private static create(snotify: SnotifyToast): number;
   /**
    * Create toast with Success style, returns toast id;
    * @param body {String}
@@ -25,7 +24,7 @@ export interface SnotifyService extends Vue {
    * @param config {SnotifyConfig}
    * @return {number}
    */
-  success(body: string, title?: string, config?: SnotifyConfig): number;
+  static success(body: string, title?: string, config?: SnotifyConfig): number;
   /**
    * Create toast with Error style, returns toast id;
    * @param body {String}
@@ -33,7 +32,7 @@ export interface SnotifyService extends Vue {
    * @param config {SnotifyConfig}
    * @return {number}
    */
-  error(body: string, title?: string, config?: SnotifyConfig): number;
+  static error(body: string, title?: string, config?: SnotifyConfig): number;
   /**
    * Create toast with Info style, returns toast id;
    * @param body {String}
@@ -41,7 +40,7 @@ export interface SnotifyService extends Vue {
    * @param config {SnotifyConfig}
    * @return {number}
    */
-  info(body: string, title?: string, config?: SnotifyConfig): number;
+  static info(body: string, title?: string, config?: SnotifyConfig): number;
   /**
    * Create toast with Warining style, returns toast id;
    * @param body {String}
@@ -49,7 +48,7 @@ export interface SnotifyService extends Vue {
    * @param config {SnotifyConfig}
    * @return {number}
    */
-  warning(body: string, title?: string, config?: SnotifyConfig): number;
+  static warning(body: string, title?: string, config?: SnotifyConfig): number;
   /**
    * Create toast without style, returns toast id;
    * @param body {String}
@@ -57,7 +56,7 @@ export interface SnotifyService extends Vue {
    * @param config {SnotifyConfig}
    * @return {number}
    */
-  simple(body: string, title?: string, config?: SnotifyConfig): number;
+  static simple(body: string, title?: string, config?: SnotifyConfig): number;
   /**
    * Create toast with Confirm style {with two buttons}, returns toast id;
    * @param body {String}
@@ -65,7 +64,7 @@ export interface SnotifyService extends Vue {
    * @param config {SnotifyConfig}
    * @return {number}
    */
-  confirm(body: string, title?: string, config?: SnotifyConfig): number;
+  static confirm(body: string, title?: string, config?: SnotifyConfig): number;
   /**
    * Create toast with Prompt style {with two buttons}, returns toast id;
    * @param body {String}
@@ -73,7 +72,7 @@ export interface SnotifyService extends Vue {
    * @param config {SnotifyConfig}
    * @return {number}
    */
-  prompt(body: string, title?: string, config?: SnotifyConfig): number;
+  static prompt(body: string, title?: string, config?: SnotifyConfig): number;
   /**
    * Creates async toast with Info style. Pass action, and resolve or reject it.
    * @param body {String}
@@ -81,12 +80,12 @@ export interface SnotifyService extends Vue {
    * @param action {Promise<SnotifyConfig> | Observable<SnotifyConfig>}
    * @return {number}
    */
-  async(body: string, title: string, action: () => Promise<SnotifyConfig>): number;
+  static async(body: string, title: string, action: () => Promise<SnotifyConfig>): number;
   /**
    * Creates empty toast with html string inside
    * @param {string | SafeHtml} html
    * @param {SnotifyConfig} config
    * @returns {number}
    */
-  html(html: string, config?: SnotifyConfig): number;
+  static html(html: string, config?: SnotifyConfig): number;
 }
