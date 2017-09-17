0d0b2dc9515501e1069d71af5d5d44799728696e
artemsky <mr.artemsky@gmail.com>
2017-09-18 01:22:31 +0300

    update deploy

--- update-docs.sh
+++ update-docs.sh
@@ -1,9 +1,13 @@
+rm -rf demo
+npm run lib:build
+npm run link:dist
+npm run example:build
 cd docs
 rm -rf _book
 gitbook install
 gitbook build ./ ../demo/documentation
 rm -rf _book
-cd ../demo/documentation
+cd ../demo
 git init
 git add -A
 git commit -m 'update demo'
