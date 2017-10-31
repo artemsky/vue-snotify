92fe0d18b4eec3e1b25e4d460d6d36fdd8b166ee
artemsky <mr.artemsky@gmail.com>
2017-11-01 00:23:20 +0200

    remove eslint && js loaders && vue-loaders

--- example/build/webpack.base.conf.js
+++ example/build/webpack.base.conf.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const utils = require('./utils');
 const config = require('../config');
-const vueLoaderConfig = require('./vue-loader.conf');
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -28,25 +27,6 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.(js|vue)$/,
-      //   loader: 'eslint-loader',
-      //   enforce: 'pre',
-      //   include: [resolve('src'), resolve('test')],
-      //   options: {
-      //     formatter: require('eslint-friendly-formatter')
-      //   }
-      // },
-      // {
-      //   test: /\.vue$/,
-      //   loader: 'vue-loader',
-      //   options: vueLoaderConfig
-      // },
-      // {
-      //   test: /\.js$/,
-      //   loader: 'babel-loader',
-      //   include: [resolve('src'), resolve('test')]
-      // },
       {
         test: /\.ts$/,
         enforce: 'pre',
---
+++ src/interfaces/index.ts
@@ -0,0 +1,9 @@
+export { SnotifyService } from './SnotifyService';
+export { Snotify } from './Snotify.interface';
+export { SnotifyAnimate } from './SnotifyAnimate.interface';
+export { SnotifyButton } from './SnotifyButton.interface';
+export { SnotifyDefaults } from './SnotifyDefaults.interface';
+export { SnotifyGlobalConfig } from './SnotifyGlobalConfig.interface';
+export { SnotifyNotifications } from './SnotifyNotifications.interface';
+export { SnotifyStyles } from './SnotifyStyles.interface';
+export { SnotifyToastConfig } from './SnotifyToastConfig.interface';
